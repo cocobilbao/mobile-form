82638956f79005a36c5049a853c333282633738b
cocobilbao <cocobilbao@gmail.com>
2023-12-14 18:01:14 +0100

    feat: add crenn banner animation

--- src/components/BannerGreen/BannerGreen.jsx
+++ src/components/BannerGreen/BannerGreen.jsx
@@ -11,15 +11,10 @@ const BannerGreen = () => {
 
   return (
     <div className="banner-green-content">
-      <div
-        className={
-          showBanner
-            ? "banner-green-square banner-green-rectangle"
-            : "banner-green-square"
-        }
-      >
-        <img src="Vector.png" alt="bombilla"/>
-        <span className={showBanner ? "banner-green-text hidden" : "hidden"}>
+      <div className="banner-green-square">
+        <img src="Vector_2.png" alt="lightbulb" className="yellow-lightbulb"/>
+        <img src="Vector.png" alt="lightbulb"  style={{position: "absolute"}} />
+        <span className="banner-green-text">
           La puerta principal -el punto más vulnerable- tiene protección
           reforzada para detectar al intruso antes de que entre.
         </span>
